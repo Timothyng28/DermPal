b5a893c0c5f8cd4b5d876d43d192e906664a416a
tmotluke <timothyng28@gmail.com>
2024-12-22 19:44:33 +0800

    Modified OurTeam Contact Icons to only render if exists

--- src/pages/Our Team/TeamCard.jsx
+++ src/pages/Our Team/TeamCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import Grid2 from '@mui/material/Grid2';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import Grid2 from "@mui/material/Grid2";
 
 function TeamCard(props) {
   return (
-    <Card sx={{width : 200}}>
+    <Card sx={{ width: 200 }}>
       <CardMedia
         component="img"
         height="140"
@@ -20,17 +20,40 @@ function TeamCard(props) {
         <Typography variant="body2" color="text.secondary">
           {props.role}
         </Typography>
-        <div style = {{height : '10px'}} />
-        <Typography variant="body2" color="black" style={{height : '150px'}}>
-            SHORT write up HELLO HI I LOVE IT HERE HAHAHHA LOL
+        <div style={{ height: "10px" }} />
+        <Typography variant="body2" color="black" style={{ height: "150px" }}>
+          SHORT write up HELLO HI I LOVE IT HERE HAHAHHA LOL
         </Typography>
-        <Grid2 container spacing={2} alignItems='center' justifyContent='center'>
-            <a href = {"mailto:" + props.email} style = {{textDecoration : 'none'}}>
-            <EmailIcon style={{ color: '#D44638', fontSize: '30px', marginLeft: '10px' }} />
-            </ a>
-            <a href = {props.linkedin} target ="_blank" rel="noopener noreferrer" style = {{textDecoration : 'none'}}>
-                <LinkedInIcon style={{ color: '#0077B5', fontSize: '30px' }} />
+        <Grid2
+          container
+          spacing={2}
+          alignItems="center"
+          justifyContent="center"
+        >
+          {props.email && (
+            <a
+              href={"mailto:" + props.email}
+              style={{ textDecoration: "none" }}
+            >
+              <EmailIcon
+                style={{
+                  color: "#D44638",
+                  fontSize: "30px",
+                  marginLeft: "10px",
+                }}
+              />
             </a>
+          )}
+          {props.linkedin && (
+            <a
+              href={props.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none" }}
+            >
+              <LinkedInIcon style={{ color: "#0077B5", fontSize: "30px" }} />
+            </a>
+          )}
         </Grid2>
       </CardContent>
     </Card>

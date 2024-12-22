90dce082b0f7ce1939fe22cdac2bbe8830122c7a
tmotluke <timothyng28@gmail.com>
2024-12-22 22:50:06 +0800

    Create Footer for future use

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import theme from "../../theme";
+
+function Footer() {
+return (
+    <footer style={{ position: "fixed", bottom: 0, width: "100%", background: theme.palette.gradient.main }}>
+            <p>its kinda ugly...</p>
+    </footer>
+);
+}
+
+export default Footer;
--- src/components/Header/NavbarStyles.js
+++ src/components/Header/NavbarStyles.js
@@ -1,58 +1,56 @@
-import { styled } from '@mui/system';
-import AppBar from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Box from '@mui/material/Box';
-
+import { styled } from "@mui/system";
+import AppBar from "@mui/material/AppBar";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Box from "@mui/material/Box";
 
 export const StyledBox = styled(Box)({
-    display: 'flex',
-    justifyContent: 'center',
-    allignItems: 'center',
-    flexGrow: 1,
-    });
+  display: "flex",
+  justifyContent: "center",
+  allignItems: "center",
+  flexGrow: 1,
+});
 
 export const StyledAppBar = styled(AppBar)(({ theme }) => ({
-    background: theme.palette.gradient.main,
-    color: '#fff',
+  background: theme.palette.gradient.main,
+  color: "#fff",
 }));
 
-export const NavButton = styled('button')({
-    color: 'black',
-    backgroundColor: 'transparent',
-    marginLeft: '10px',
-    border: 'none',
-    fontSize: '1.1rem',
-    fontWeight: 'bold',
-    '&:hover': {
-        cursor: 'pointer',
-        color: 'teal',
-    },
+export const NavButton = styled("button")({
+  color: "black",
+  backgroundColor: "transparent",
+  marginLeft: "10px",
+  border: "none",
+  fontSize: "1.1rem",
+  fontWeight: "bold",
+  "&:hover": {
+    cursor: "pointer",
+    color: "teal",
+  },
 });
 
 export const DropDown = styled(Menu)({
-  backgroundColor: 'transparent',
-  color: 'black',
-  borderRadius: '5px',
-  position: 'absolute',
-  top: '22px',
-  right: '0px',
-    zIndex: 1300,
-    padding: '0px',
-    margin: '0px',
+  backgroundColor: "transparent",
+  color: "black",
+  borderRadius: "5px",
+  position: "absolute",
+  top: "22px",
+  right: "0px",
+  zIndex: 1300,
+  padding: "0px",
+  margin: "0px",
 });
 
-export const StyledMenuList = styled('ul')({
-    backgroundColor: '#e0f7fa', 
-    borderRadius: '8px', 
-    padding: '10px 0',  
-    boxShadow: '0px 5px 10px rgba(0, 0, 0, 0.15)', 
-  });
-  
+export const StyledMenuList = styled("ul")({
+  backgroundColor: "#e0f7fa",
+  borderRadius: "8px",
+  padding: "10px 0",
+  boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.15)",
+});
 
 export const StyledTypography = styled(Typography)({
-        textDecoration: 'none',
-        color: 'teal',
-        fontWeight: 'bold',
-        letterSpacing: '.2rem',
-    });
+  textDecoration: "none",
+  color: "teal",
+  fontWeight: "bold",
+  letterSpacing: ".2rem",
+});
